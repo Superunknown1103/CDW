12ccf79c28aba7a5f889b361776d0a21866f94d8
Superunknown1103 <superunknown1103@gmail.com>
2017-10-23 17:32:34 -0500

    5:32 PM

--- my-react-app/app.js
+++ my-react-app/app.js
@@ -4,10 +4,25 @@ import React, { Component } from 'react';
 class App extends Component {
    render() {
       return (
-         <div>
-            <h1>Welcome to your CDW Dashboard</h1>
-         </div>
-         // every component must be wrapped in a div.
+          <div>
+              <h1>Welcome to Chicago Dog Walkers</h1>
+              <h2>Let's check off which dogs you walked today</h2>
+        <form>
+        <input type="checkbox" id="dogcheck" className="dog" />
+        <label for="Betty">Betty</label>
+        <br />
+        <input type="checkbox" id="dogcheck" className="dog" />
+        <label for="Jackson">Jackson</label>
+        <br />
+        <input type="checkbox" id="dogcheck" className="dog" />
+        <label for="Simon">Simon</label>
+        <br />
+        <input type="checkbox" id="dogcheck" className="dog" />
+        <label for="Renny">Renny</label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+        </div>
       );
    }
 }
