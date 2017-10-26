e3904fdac59f21b9fecb890a43f240638284d2ea
Superunknown1103 <superunknown1103@gmail.com>
2017-10-26 09:47:21 -0500

    trying to get dogs to display

--- my-react-app/components/dogswalked.jsx
+++ my-react-app/components/dogswalked.jsx
@@ -1,24 +1,51 @@
 import React, { Component } from 'react';
+import Walkreport from './walkreport.jsx'
 
 export default class Dogswalked extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {dogsWalked: 0}
-        this.addDog = this.updateDogs.bind(this, 1);
-        this.subtractDog = this.updateDogs.bind(this, -1);
+        this.state = {dogsWalked: ""}
+        this.addDogs = this.updateDogs.bind(this, 1);
     }
 
 render() {
     return (
         <div>
+            <div className="wrapper">
+            <h2>Let's check off which dogs you walked today</h2>
+        <form>
+        <input type="checkbox" id="dogcheck" className="dog" />
+        <label htmlFor="Betty">Betty</label>
+        <br />
+        <input type="checkbox" id="dogcheck" className="dog" />
+        <label htmlFor="Jackson">Jackson</label>
+        <br />
+        <input type="checkbox" id="dogcheck" className="dog" />
+        <label htmlFor="Simon">Simon</label>
+        <br />
+        <input type="checkbox" id="dogcheck" className="dog" />
+        <label htmlFor="Renny">Renny</label>
+        <br />
+      </form>
+      </div>
             <div>{this.state.dogsWalked}</div>
-            <input type='button' value='+' onClick={this.addDog} />
-                <input type='button' value='-' onClick={this.subtractDog} />
+            <input type='button' value='+' onClick={this.addDogs} />
             </div>
         );
     }
     
     updateDogs(dogsWalked) {
-        this.setState({dogsWalked: this.state.dogsWalked + dogsWalked});
+        function getDogs(){
+        var inputs = document.getElementById("input");
+        for(var i = 0; i < inputs.length; i++) {
+            if(inputs[i].type == "checkbox") {
+                inputs[i].checked = true;
+            }
+            return dogs;
+            console.log(dogs);
+    }
+    this.setState({dogsWalked: this.state.dogsWalked + dogsWalked});
+    
+    }
     }
 }
--- my-react-app/components/walkreport.jsx
+++ my-react-app/components/walkreport.jsx
@@ -6,26 +6,11 @@ class Walkreport extends Component {
  
     render() {
         return (
-            <div className="wrapper">
-            <h2>Let's check off which dogs you walked today</h2>
-        <form>
-        <input type="checkbox" id="dogcheck" className="dog" />
-        <label htmlFor="Betty">Betty</label>
-        <br />
-        <input type="checkbox" id="dogcheck" className="dog" />
-        <label htmlFor="Jackson">Jackson</label>
-        <br />
-        <input type="checkbox" id="dogcheck" className="dog" />
-        <label htmlFor="Simon">Simon</label>
-        <br />
-        <input type="checkbox" id="dogcheck" className="dog" />
-        <label htmlFor="Renny">Renny</label>
-        <br />
-        <Dogswalked />
-
-      </form>
-      </div>
+            <div>
+                <Dogswalked />
+            </div>
         );
+
     }
 }
 
